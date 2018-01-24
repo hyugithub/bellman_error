fae39a3521429ae17029cec08c4be090dc8fb7d4
hyu <hyu.github@gmail.com>
2018-01-23 20:28:52 -0500

    working version -- linear

---
+++ functional_approximator.py
@@ -0,0 +1,112 @@
+#the purpose of this file is to generate a simple functional 
+# approximator to V(s,t)
+
+import numpy as np
+import tensorflow as tf
+from tensorflow.python.framework import ops
+
+ops.reset_default_graph()
+np.set_printoptions(precision=4)
+np.random.seed(4321)
+
+num_nights = 3
+capacity = 7
+# the true number of products is num_nights-1 because every products 
+# take a 2-night stay
+# product zero is the no-revenue no resource product
+# added for simplicity
+product_null = 0
+
+num_product = num_nights+1
+product_resource_map = np.zeros((num_product, num_nights))
+for i in range(1,num_product-1):
+    product_resource_map[i][i-1] = 1.0
+    product_resource_map[i][i] = 1.0
+product_resource_map[num_product-1][num_nights-1] = 1.0
+
+product_revenue = np.random.uniform(size=[num_product])
+product_revenue[product_null] = 0
+#total demand
+product_demand = np.random.uniform(size=[num_product])*capacity
+product_demand[product_null]  = 0
+
+num_steps = int(np.sum(product_demand)/0.01)
+
+#arrival rate (including)
+product_prob = np.divide(product_demand,num_steps)
+product_prob[0] = 1.0 - np.sum(product_prob)
+
+#define a state (in batch) and a linear value function
+batch_size = 5
+
+#LHS is the value function for current state at time t
+#for each state, we need num_nights real value inputs for available
+# inventory, and +1 for time
+dim_state_space = num_nights+1
+
+state_lhs = tf.placeholder(tf.float32, [batch_size, dim_state_space])
+#RHS is the value function for relevant states at time t-1
+state_rhs = tf.placeholder(tf.float32, [batch_size, num_product, dim_state_space])
+mask = tf.placeholder(tf.float32, [batch_size, num_product])
+#probability
+
+#define linear approximation parameters
+weights = tf.Variable(np.random.uniform(size=[dim_state_space]), dtype=tf.float32)
+bias = tf.Variable(np.random.uniform(size=1), dtype=tf.float32)
+
+#define LHS
+value_lhs = tf.multiply(state_lhs, weights)
+value_lhs = tf.reduce_sum(value_lhs, axis=1) + bias
+
+#define RHS
+value_rhs = tf.multiply(state_rhs, weights)
+#we need the mask here because certain products are unsellable given
+#a certain state. To implement this logic, we do two things:
+# 1. setting mask = 0 for such state/product combination
+# 2. in data preparation setting that state to 0 
+#in this way, no error should come up in approximator
+#and no impact on gradient estimator
+value_rhs = tf.multiply(mask, tf.reduce_sum(value_rhs, axis=2) + bias)
+value_rhs = tf.maximum(value_rhs + 
+                       tf.constant(product_revenue, dtype=tf.float32) - 
+                       tf.reshape(value_lhs, [-1,1]), tf.constant(0.0))
+value_rhs = tf.multiply(value_rhs
+                        , tf.constant(product_prob
+                                      , dtype=tf.float32))
+value_rhs = tf.reduce_sum(value_rhs, axis=1)
+
+bellman_error = value_lhs-value_rhs
+loss = tf.reduce_sum(tf.multiply(bellman_error,bellman_error))
+
+train_step = tf.train.AdagradOptimizer(0.3).minimize(loss)    
+
+#gw = tf.gradients(loss, weights)
+#gb = tf.gradients(loss, bias)
+
+#generate data for LHS
+#state_lhs = tf.placeholder(tf.float32, [batch_size, dim_state_space])
+data_lhs = np.random.choice(capacity+1, [batch_size, num_nights])
+time_lhs = np.random.choice(range(1,num_steps), [batch_size,1])
+data_lhs = np.hstack([data_lhs, time_lhs])
+
+#derive RHS
+# after consuming every product, what's the RHS
+#batch x product x night(state)
+#resource_consumed = np.swapaxes(np.tile(product_resource_map, (batch_size,1,1)), 1,2)
+resource_consumed = np.tile(product_resource_map, (batch_size,1,1))
+data_rhs = np.reshape(data_lhs, (batch_size,num_product,-1)) - resource_consumed
+data_mask = (1-np.any(data_rhs<0, axis=2)).astype(int)
+
+data_rhs = np.maximum(data_rhs ,0)
+time_rhs = time_lhs-1
+time_rhs = np.reshape(np.repeat(np.ravel(time_rhs),num_product), (batch_size,num_product,-1))
+data_rhs = np.concatenate([data_rhs, time_rhs], axis=2)
+
+with tf.Session() as sess:    
+    sess.run(tf.global_variables_initializer())
+    result = sess.run([loss]
+            , feed_dict={state_lhs: data_lhs,
+                         state_rhs: data_rhs,
+                         mask: data_mask
+                         })
+    print(result)
--- gradient_test.py
+++ gradient_test.py
@@ -1,3 +1,6 @@
+#the purpose of this file is to test feasibility of a bellman
+#error. 
+
 import numpy as np
 import tensorflow as tf
 
@@ -23,9 +26,19 @@ f3 = tf.maximum(f1,f2)
 gw1 = tf.gradients(f3,W1)
 gw2 = tf.gradients(f3,W2)
 
+# sample code to optimize gradients
+#params = tf.trainable_variables()
+#gradients = tf.gradients(train_loss, params)
+#clipped_gradients, _ = tf.clip_by_global_norm(gradients
+#                                              , max_gradient_norm)
+
+
+
 #tf.global_variables_initializer()
 
 with tf.Session() as sess:    
     sess.run(tf.global_variables_initializer())
     result = sess.run([f3,gw1,gw2], feed_dict={x1:dx1,x2:dx2})
     print(result)
+
+
