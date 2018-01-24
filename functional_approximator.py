2046912f6f73fd3beb1cf05245b863c329c3f2c9
hyu <hyu.github@gmail.com>
2018-01-23 23:25:48 -0500

    fixed half the bug; end of day check in

--- functional_approximator.py
+++ functional_approximator.py
@@ -12,20 +12,24 @@ ops.reset_default_graph()
 np.set_printoptions(precision=4)
 np.random.seed(4321)
 
-num_nights = 4
+num_nights = 3
 capacity = 10
-# the true number of products is num_nights-1 because every products 
-# take a 2-night stay
+
 # product zero is the no-revenue no resource product
 # added for simplicity
 product_null = 0
-
-num_product = num_nights+1
+# unfortunately, to avoid confusion we need to add a fairly 
+# complex product matrix
+# if there are N nights, there are N one-night product from 
+# 1 to N; there are also N-1 two-night products from N+1 to 2N-1
+num_product = num_nights*2
 product_resource_map = np.zeros((num_product, num_nights))
-for i in range(1,num_product-1):
+for i in range(1,num_nights):
     product_resource_map[i][i-1] = 1.0
     product_resource_map[i][i] = 1.0
-product_resource_map[num_product-1][num_nights-1] = 1.0
+for i in range(0,num_nights):    
+    product_resource_map[i+num_nights][i] = 1.0
+#product_resource_map[num_product-1][num_nights-1] = 1.0
 
 product_revenue = np.random.uniform(size=[num_product])
 product_revenue[product_null] = 0
@@ -47,9 +51,12 @@ batch_size = 5
 # inventory, and +1 for time
 dim_state_space = num_nights+1
 
+#V(s,t)
 state_lhs = tf.placeholder(tf.float32, [batch_size, dim_state_space])
-#RHS is the value function for relevant states at time t-1
+#V(s-a(p),t-1)
 state_rhs_1 = tf.placeholder(tf.float32, [batch_size, num_product, dim_state_space])
+#V(s,t-1)
+state_rhs_2 = tf.placeholder(tf.float32, [batch_size, dim_state_space])
 mask = tf.placeholder(tf.float32, [batch_size, num_product])
 #probability
 
@@ -61,24 +68,27 @@ bias = tf.Variable(np.random.uniform(size=1), dtype=tf.float32)
 value_lhs = tf.multiply(state_lhs, weights)
 value_lhs = tf.reduce_sum(value_lhs, axis=1) + bias
 
-#define RHS
-value_rhs = tf.multiply(state_rhs_1, weights)
+#V(s,t-1) as a matrix of batch x 1
+value_rhs_2 = tf.reduce_sum(tf.multiply(state_rhs_2, weights), axis=1) + bias
+
+# this is a long definition for the max+ calculation
+#V(s-a(p),t-1) for every p, dimension is batch x product
+value_rhs_1 = tf.reduce_sum(tf.multiply(state_rhs_1, weights), axis=2) + bias
+
+value_rhs_1 = tf.maximum(value_rhs_1 - tf.reshape(value_rhs_2, [batch_size,-1]),
+                         tf.constant(0.0))
 #we need the mask here because certain products are unsellable given
 #a certain state. To implement this logic, we do two things:
 # 1. setting mask = 0 for such state/product combination
 # 2. in data preparation setting that state to 0 
 #in this way, no error should come up in approximator
 #and no impact on gradient estimator
-#value_rhs = tf.multiply(mask, tf.reduce_sum(value_rhs, axis=2) + bias)
-value_rhs = tf.reduce_sum(value_rhs, axis=2) + bias
-value_rhs = tf.maximum(value_rhs + 
-                       tf.constant(product_revenue, dtype=tf.float32) - 
-                       tf.reshape(value_lhs, [-1,1]), tf.constant(0.0))
-value_rhs = tf.multiply(value_rhs, mask)
-value_rhs = tf.multiply(value_rhs
+value_rhs_1 = tf.multiply(value_rhs_1, mask)
+value_rhs_1 = tf.multiply(value_rhs_1
                         , tf.constant(product_prob
                                       , dtype=tf.float32))
-value_rhs = tf.reduce_sum(value_rhs, axis=1)
+value_rhs_1 = tf.reduce_sum(value_rhs_1, axis=1)
+value_rhs = value_rhs_2 - value_rhs_1
 
 bellman_error = value_lhs-value_rhs
 loss = tf.reduce_mean(tf.multiply(bellman_error,bellman_error))
@@ -88,35 +98,41 @@ train_step = tf.train.AdagradOptimizer(0.3).minimize(loss)
 #gw = tf.gradients(loss, weights)
 #gb = tf.gradients(loss, bias)
 
-num_batches = 2500
+num_batches = 1000
 with tf.Session() as sess:    
     sess.run(tf.global_variables_initializer())
     for batch in range(num_batches):
         #generate data for LHS
         #state_lhs = tf.placeholder(tf.float32, [batch_size, dim_state_space])
-        data_lhs = np.random.choice(capacity+1, [batch_size, num_nights])
+        data_lhs_0 = np.random.choice(capacity+1, [batch_size, num_nights])
         time_lhs = np.random.choice(range(1,num_steps), [batch_size,1])
-        data_lhs = np.hstack([data_lhs, time_lhs])
+        
+        data_rhs_2 = np.hstack([data_lhs_0, time_lhs-1])
+        data_lhs = np.hstack([data_lhs_0, time_lhs])
         
         #derive RHS
         # after consuming every product, what's the RHS
         #batch x product x night(state)
         #resource_consumed = np.swapaxes(np.tile(product_resource_map, (batch_size,1,1)), 1,2)
         resource_consumed = np.tile(product_resource_map, (batch_size,1,1))
-        data_rhs = np.reshape(data_lhs, (batch_size,num_product,-1)) - resource_consumed
-        data_mask = (1-np.any(data_rhs<0, axis=2)).astype(int)
-        
-        data_rhs = np.maximum(data_rhs ,0)
+        #data_rhs_1 = np.reshape(data_lhs_0, (batch_size,num_product,-1)) - resource_consumed
+        data_rhs_1 = np.repeat(data_lhs_0.flatten(), num_product)
+        data_rhs_1 = np.reshape(data_rhs_1, (batch_size, num_nights, -1))
+        data_rhs_1 = np.swapaxes(data_rhs_1, 1,2,) - resource_consumed
+        data_mask = (1-np.any(data_rhs_1<0, axis=2)).astype(int)        
+        data_rhs_1 = np.maximum(data_rhs_1, 0)
         time_rhs = time_lhs-1
         time_rhs = np.reshape(np.repeat(np.ravel(time_rhs),num_product), (batch_size,num_product,-1))
-        data_rhs = np.concatenate([data_rhs, time_rhs], axis=2)        
+        data_rhs_1 = np.concatenate([data_rhs_1, time_rhs], axis=2)  
+        
         result, result_weights, result_bias, _ = sess.run([loss, weights, bias, train_step]
                 , feed_dict={state_lhs: data_lhs,
-                             state_rhs_1: data_rhs,
+                             state_rhs_1: data_rhs_1,
+                             state_rhs_2: data_rhs_2,
                              mask: data_mask
                              })
         if batch % 100 == 0:    
             print("batch = ", batch, " result = %.2f"%result)
-            print("weights = ", result_weights, " bias = ", result_bias)
+            #print("weights = ", result_weights, " bias = ", result_bias)
 
 print("total program time = %.2f seconds" % (time.time()-ts))
