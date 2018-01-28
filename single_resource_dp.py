40d9f090436c06f90ca3ebb246189a382abb767c
hyu <hyu.github@gmail.com>
2018-01-27 20:22:26 -0500

    regular checkin before adding lp and dual value adjustment

--- single_resource_dp.py
+++ single_resource_dp.py
@@ -8,33 +8,16 @@ import numpy as np
 import time
 np.set_printoptions(precision=2)
 
-def single_resource_dp(num_product, num_steps, cap, product_prob, adj_revenue):
+#def single_resource_dp(num_product, num_steps, cap, product_prob, adj_revenue, resource ):
+
+def single_resource_dp():
     # num_product scalar 
     # num_step scalar for now
     # cap is scalar
     # product_prob is num_product x 1
     # adj_revenue is num_product x 1
-    
-    #temporary inputs
-    num_product = 28
-    num_steps = 128900
-    #num_steps = 1289
-    cap = 100
-    product_prob = np.asarray([9.9000e-01, 6.0436e-05, 5.9443e-04, 6.6212e-04, 1.1665e-04,
-       7.8424e-05, 2.1006e-04, 2.3389e-05, 6.4966e-04, 4.6338e-04,
-       7.2199e-04, 3.8423e-04, 2.9192e-04, 1.8903e-04, 4.6666e-04,
-       2.5565e-04, 3.5800e-04, 5.6212e-04, 6.2947e-04, 7.6461e-04,
-       1.7601e-04, 4.4020e-04, 3.0932e-05, 7.2659e-04, 2.1034e-04,
-       4.4593e-04, 2.8384e-04, 2.0397e-04])
-    adj_revenue = np.asarray([  0.    , 815.064 , 767.905 , 286.3545, 193.0943, 978.9122,
-       406.2287, 757.7679,  89.1518, 309.8835, 618.9276, 459.9105,
-       218.3094, 663.5202, 678.6827, 950.3185, 281.2611, 619.8517,
-       383.2951, 400.3607, 942.6527, 929.9179, 948.3751, 375.4859,
-       342.2961, 664.7771,  42.3206, 232.2399])
-    
-    resource = np.ones(num_product)
-    resource[0] = 0
-    
+    # resource is num_product x 1
+        
     # according to RM literature, tstep = 0 means no time left    
     
     ts = time.time()
@@ -47,19 +30,18 @@ def single_resource_dp(num_product, num_steps, cap, product_prob, adj_revenue):
         #V(x,t) = sum [Pr(p)*max(price(p)+V(x-a(p),t-1), V(x,t-1))]
         if 1:    
             #x-
-            x = np.reshape(np.repeat(range(0,cap+1), num_product), (-1, num_product))
-            x = x - resource
+            x = np.reshape(np.repeat(range(0,cap+1), num_product)
+                        , (-1, num_product)) - resource
             
             #note: before flatten it is inv level x product
             y = np.maximum(x,0).astype(int).flatten()
             
             lhs = np.reshape(V_tm1[y], (cap+1, num_product)) + adj_revenue
-            flag = (x>=0.0).astype(int)
-            lhs = np.multiply(lhs,flag)
+            lhs = np.multiply(lhs, (x>=0.0).astype(int))
             # if flag = 0, V(x-a(p)) is invalid and the max(a,b)
             # is invalid too. 
-            rhs = V_tm1
-            rev = np.maximum(lhs, np.reshape(rhs, (rhs.shape[0],-1)))        
+            #rhs = V_tm1
+            rev = np.maximum(lhs, np.reshape(V_tm1, (V_tm1.shape[0],-1)))        
             #print(rev)
             
             #expectation over all products
@@ -76,7 +58,6 @@ def single_resource_dp(num_product, num_steps, cap, product_prob, adj_revenue):
                 #print(np.dot(product_prob, rev))
                 V_t[x] = np.dot(product_prob, rev)
             V_tm1 = V_t
-        #print(V_tm1)
         result[tstep] = V_tm1
 
     print("min = %.2f"%np.amin(V_tm1)
@@ -89,4 +70,24 @@ def single_resource_dp(num_product, num_steps, cap, product_prob, adj_revenue):
         # V(x,t) = E[max (price(p)u + V(x-u,t-1))]
         # V(s,t) = max
     
-    
+num_product = 28
+num_steps = 128900
+#num_steps = 1289
+cap = 1000
+product_prob = np.asarray([9.9000e-01, 6.0436e-05, 5.9443e-04, 6.6212e-04, 1.1665e-04,
+   7.8424e-05, 2.1006e-04, 2.3389e-05, 6.4966e-04, 4.6338e-04,
+   7.2199e-04, 3.8423e-04, 2.9192e-04, 1.8903e-04, 4.6666e-04,
+   2.5565e-04, 3.5800e-04, 5.6212e-04, 6.2947e-04, 7.6461e-04,
+   1.7601e-04, 4.4020e-04, 3.0932e-05, 7.2659e-04, 2.1034e-04,
+   4.4593e-04, 2.8384e-04, 2.0397e-04])
+adj_revenue = np.asarray([  0.    , 815.064 , 767.905 , 286.3545, 193.0943, 978.9122,
+   406.2287, 757.7679,  89.1518, 309.8835, 618.9276, 459.9105,
+   218.3094, 663.5202, 678.6827, 950.3185, 281.2611, 619.8517,
+   383.2951, 400.3607, 942.6527, 929.9179, 948.3751, 375.4859,
+   342.2961, 664.7771,  42.3206, 232.2399])
+
+resource = np.ones(num_product)
+#product null
+resource[0] = 0
+
+single_resource_dp()
